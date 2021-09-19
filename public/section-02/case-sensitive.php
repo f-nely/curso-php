5a8ac2e662617ba4544d6f608d2d1d7bd1634961
nely de faria <nelly.nf54@gmail.com>
2021-09-19 10:17:56 -0300

    Criando arquiov case-sensitive.php

---
+++ public/section-02/case-sensitive.php
@@ -0,0 +1,13 @@
+<?php
+	
+// não é case sensitive
+ echo "teste <br>";
+eCHO "testando 2 <br>";
+ECHO "testando 3 <br>";
+
+// é case sensitive
+$name = 'Matheus';
+echo $name . '<br>';
+//echo $NAME . '<br>'; 
+
+
