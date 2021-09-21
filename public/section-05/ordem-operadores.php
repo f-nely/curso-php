c06ed6ceb7ab3bc5587b699478190ba32d0148c1
nely de faria <nelly.nf54@gmail.com>
2021-09-21 16:23:30 -0300

    Criando arquivo ordem-operadores.php

---
+++ public/section-05/ordem-operadores.php
@@ -0,0 +1,15 @@
+<?php
+
+echo 3 + 5 * 5 . '<br>';
+echo (3 + 5) * 5 . '<br>';
+echo 5 + 2 / 5 . '<br>';
+
+$a = 5;
+$b = 2;
+$c = 10;
+
+echo $a + $b / $c . '<br>';
+echo $c + $b / $a . '<br>';
+
+$d = $a * $b * $c;
+echo $d . '<br>';
