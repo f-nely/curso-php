29efbcdde6a67858f5b449b25d486ae4daba4651
nely de faria <nelly.nf54@gmail.com>
2021-09-21 16:51:49 -0300

    Criando arquivo operadores-aritmeticos.php

---
+++ public/section-05/operadores-aritmeticos.php
@@ -0,0 +1,13 @@
+<?php
+
+$soma = 5 + 5;
+echo "$soma <br>";
+
+$subtracao = 10 - 2;
+echo "$subtracao <br>";
+
+$multiplicacao = 2 * 199;
+echo "$multiplicacao <br>";
+
+$divisao = 123 / 48;
+echo "$divisao <br>";
