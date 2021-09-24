751a0089719a99ed58bb9cbad955402c6ccc72a2
nely de faria <nelly.nf54@gmail.com>
2021-09-24 16:13:02 -0300

    Criando arquivo auto-inc-dec.php

---
+++ public/section-05/auto-inc-dec.php
@@ -0,0 +1,25 @@
+<?php
+
+$x = 0;
+$y = 0;
+
+echo "x: $x <br>";
+echo "y: $y <br>";
+
+$x++;
+$y = $y + 1;
+
+echo "x: $x <br>";
+echo "y: $y <br><br>";
+
+$n = 10;
+$m = 10;
+
+echo "n: $n <br>";
+echo "m: $m <br>";
+
+$n--;
+$m = $m - 1;
+
+echo "n: $n <br>";
+echo "m: $m <br>";
