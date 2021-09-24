55e900c0feee4112d9f85e3b377018594c12cbe6
nely de faria <nelly.nf54@gmail.com>
2021-09-24 16:30:01 -0300

    Criando arquivo ex016.php

---
+++ public/section-05/ex016.php
@@ -0,0 +1,24 @@
+<?php
+
+$a = 5;
+$b = 3;
+
+// igualdade
+if($a == $b) {
+  echo "A é igual a B <br>";
+}
+
+// diferença
+if($a != $b) {
+  echo "A é diferente a B <br>";
+}
+
+// idêntico
+if($a === $b) {
+  echo "A é idêntico a B <br>";
+}
+
+// não idêntico
+if($a !== $b) {
+  echo "A é não idêntico a B <br>";
+}
