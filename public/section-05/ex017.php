ab7e59551af15689a57d83124b75058d760fa145
nely de faria <nelly.nf54@gmail.com>
2021-09-24 20:22:47 -0300

    Criando arquivo ex017.php

---
+++ public/section-05/ex017.php
@@ -0,0 +1,13 @@
+<?php
+
+if(15 > 5 && 'james' === 'james') {
+  echo "a comparação 1 é verdadeira <br>";
+}
+
+if('teste' > 5 && 1) {
+  echo "a comparação 2 é verdadeira<br>";
+}
+
+if(2 === 3 && 5 >= 3) {
+  echo "a comparação 3 é verdadeira3 <br>";
+}
