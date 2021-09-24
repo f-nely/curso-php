cad9594882bed59e93dcfa2507953a2122636855
nely de faria <nelly.nf54@gmail.com>
2021-09-24 20:23:25 -0300

    Criando arquivo ex018.php

---
+++ public/section-05/ex018.php
@@ -0,0 +1,13 @@
+<?php
+
+if(12 < 5 || 'Taylor' === 'Taylor') { // false e true
+  echo "A operação 1 é verdadeira <br>";
+}
+
+if(1 > 5 || 1) { // false e true
+  echo "A operação 2 é verdadeira <br>";
+}
+
+if(20 === '20' && 51 >= 31) { // false e true
+  echo "A operação 3 é verdadeira <br>";
+}
