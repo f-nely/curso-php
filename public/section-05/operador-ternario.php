9d633982cd9b34ac74a92c29420d4f619a203c03
nely de faria <nelly.nf54@gmail.com>
2021-09-25 11:44:44 -0300

    Criando arquivo operador-ternario.php

---
+++ public/section-05/operador-ternario.php
@@ -0,0 +1,12 @@
+<?php
+
+// true
+echo 20 > 10 ? 'Deu true <br>' : 'Deu false <br>';
+
+// false
+echo 20 > 50 ? 'Deu true <br>' : 'Deu false <br>';
+
+$a = 10;
+$b = 5;
+
+echo $a >= $b ? 'A é maior que B' : 'B é maior que A';
