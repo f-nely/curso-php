719736ed6bfe39c25e23776b36720e8c0bb9160d
nely de faria <nelly.nf54@gmail.com>
2021-09-25 11:46:51 -0300

    Alterando arquivo ex020.php

--- public/section-05/ex020.php
+++ public/section-05/ex020.php
@@ -3,4 +3,4 @@
 $n1 = 41;
 $n2 = 17;
 
-echo $n1 >= $n2 ? "$n1 é maior que $n2 <br>" : "$n1 é menor que $n2 <br>";
+echo $n1 >= $n2 ? "$n1 é maior ou igual a $n2 <br>" : "$n2 é maior ou igual a $n1 <br>";
