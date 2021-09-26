6ef0febba29161b9511af2161d99f28b0388b75a
nely de faria <nelly.nf54@gmail.com>
2021-09-26 10:03:45 -0300

    Criando arquivo ex021.php

---
+++ public/section-06/ex021.php
@@ -0,0 +1,22 @@
+<?php
+
+$a = 5;
+$b = 2;
+
+if($a > $b) {
+  echo "$a é maior que $b <br>";
+}
+
+$name1 = 'Matheus';
+$name2 = 'Pedro';
+
+if($name1 != $name2) {
+  echo "$name1 é direfente de $name2 <br>";
+}
+
+$n1 = 12;
+$n2 = 11;
+
+if($n1 <= $n2) {
+  echo "$n1 é menor ou igual a $n2 <br>";
+}
