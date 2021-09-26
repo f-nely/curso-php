5594bca370a03bfeaf792d612a1c979afb18b624
nely de faria <nelly.nf54@gmail.com>
2021-09-26 10:03:00 -0300

    Criando arquivo estrutura-if.php

---
+++ public/section-06/estrutura-if.php
@@ -0,0 +1,19 @@
+<?php
+
+// condição verdadeira
+if(5 > 2) {
+  echo "Deu certo! Entrou no if <br>";
+}
+
+// utilizando op. lógico
+if(10 === 10 && 9 > 3) {
+  echo "Deu certo! Entrou no if 2 <br>";
+}
+
+// variáveis
+$a = 10;
+$b = 5;
+
+if($a >= $b) {
+  echo "$a é maior que $b <br>";
+}
