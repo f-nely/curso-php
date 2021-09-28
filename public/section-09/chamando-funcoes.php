10615961a679e4b1db25a10d007bd164246d056e
nely de faria <nelly.nf54@gmail.com>
2021-09-28 18:36:51 -0300

    Criando arquivo chamando-funcoes.php

---
+++ public/section-09/chamando-funcoes.php
@@ -0,0 +1,16 @@
+<?php
+
+// definindo função
+function soma(): void
+{
+  $a = 4;
+  $b = 2;
+
+  echo $a + $b . '<br>';
+}
+
+// chamando função
+soma();
+
+// função do PHP
+echo strtoupper('curso de php');
