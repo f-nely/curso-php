3e21f71585aac3e2a3d66c9f15a2db8ebc8aecd4
nely de faria <nelly.nf54@gmail.com>
2021-09-28 18:13:58 -0300

    Criando arquivo exibicao-conteudo.php

---
+++ public/section-08/exibicao-conteudo.php
@@ -0,0 +1,14 @@
+<?php
+
+$name = 'Taylor';
+
+?>
+
+<form action="">
+    <div>
+        <input type="text" name="name" value="<?= $name; ?>">
+    </div>
+    <div>
+        <input type="submit" value="Enviar">
+    </div>
+</form>
