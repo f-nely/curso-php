89f01eabc7e354d184f157e365a397c8e11384c6
nely de faria <nelly.nf54@gmail.com>
2021-09-30 17:58:59 -0300

    Criando arquivo valores-de-escape.php

---
+++ public/section-10/valores-de-escape.php
@@ -0,0 +1,15 @@
+<?php
+
+header("Content-type: text/plain");
+
+// pular linha
+echo "Isso aqui vai ficar na primeira linha \n E isso na segunda linha \n";
+
+// tab
+echo "Testando o tab \t aqui \n";
+
+// barra ivertida
+echo "Barra invertida \\";
+
+// dolar
+echo "\nImprimindo o dólar \$a";
