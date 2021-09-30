31fae7c32764cd4ded614c5fd6266be2a8e3ca90
nely de faria <nelly.nf54@gmail.com>
2021-09-30 19:44:12 -0300

    Criando arquivo case-de-palavras.php

---
+++ public/section-10/case-de-palavras.php
@@ -0,0 +1,10 @@
+<?php
+
+$frase = "testando o case de uma palavra <br>";
+$frase2 = "testando o case de uma palavra <br>";
+
+// primeira letra em miúsculos
+echo ucfirst($frase);
+
+// todas as palavras com as iniciais maiúsculas
+echo ucwords($frase2);
