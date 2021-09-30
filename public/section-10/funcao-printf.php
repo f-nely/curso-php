543b7fb8f7d9f66f3c2a9d5c557251e463681951
nely de faria <nelly.nf54@gmail.com>
2021-09-30 18:18:28 -0300

    Criando arquivo funcao-printf.php

---
+++ public/section-10/funcao-printf.php
@@ -0,0 +1,13 @@
+<?php
+
+// %s - string
+$nome = "Taylor";
+printf("O nome é %s <br>", $nome);
+
+// %d - int
+$n = 10;
+printf("O número é %d e o outro é %d <br>", $n, 150);
+
+// %f - float
+$temperatura = 34.978001;
+printf("A temperatura atual é %.2f <br>", $temperatura);
