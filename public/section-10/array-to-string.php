40f0b160b5932830c2275a14a32a3a9eaa2533af
nely de faria <nelly.nf54@gmail.com>
2021-10-01 17:38:07 -0300

    Criando arquivo array-to-string.php

---
+++ public/section-10/array-to-string.php
@@ -0,0 +1,13 @@
+<?php
+
+$arr = ['Maçã', 'Pera', 'Mamão', 'Batata'];
+
+$str = implode(' - ', $arr);
+
+echo "$str <br>";
+
+$arr2 = ['Avião', 'Tanque', 'Barco', 'Carro'];
+
+$str2 = implode(' * ', $arr2);
+
+echo "$str2 <br>"; 
