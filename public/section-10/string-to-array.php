92f6b7e45d6eac87de2d8899fb65ea18ed9bc0dc
nely de faria <nelly.nf54@gmail.com>
2021-10-01 17:37:26 -0300

    Criando arquivo string-to-array.php

---
+++ public/section-10/string-to-array.php
@@ -0,0 +1,15 @@
+<?php
+
+$car = "Jetta, S10, Amarok, Jeep Compass";
+
+$cars = explode(', ', $car);
+
+echo "<pre>";
+print_r($cars);
+
+$fruit = "Laranja, Maçã, Uva, Limão";
+
+$fruits = explode(', ', $fruit);
+
+echo "<pre>";
+print_r($fruits);
