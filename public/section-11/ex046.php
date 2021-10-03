9d6f97197ae64bac1178640ba73b65e79db6ff28
nely de faria <nelly.nf54@gmail.com>
2021-10-03 11:50:48 -0300

    Criando arquivo ex046.php

---
+++ public/section-11/ex046.php
@@ -0,0 +1,17 @@
+<?php
+
+$arr = [
+  [1, 2, 3, 4],
+  [5, 6, 7, 8],
+  [8, 9, 10, 11]
+];
+
+// loop no array externo
+for($i = 0; $i < count($arr); $i++) {
+  //imprimindo array
+  echo "Imprimindo array externo " . ($i + 1) . "<br>";
+  for($j = 0; $j < count($arr[$i]); $j++) {
+    echo $arr[$i][$j] . '<br>';
+  }
+}
+
