844f1ba290644de379a570dc049ae2b5c01efce7
nely de faria <nelly.nf54@gmail.com>
2021-10-04 18:22:14 -0300

    Criando arquivo array-slice.php

---
+++ public/section-11/array-slice.php
@@ -0,0 +1,27 @@
+<?php
+
+$arr = [2, 4, 6, 8, 10, 12, 14, 16, 18];
+
+$slice1 = array_slice($arr, 1, 3);
+
+echo "<pre>";
+print_r($slice1);
+echo "<br>";
+
+$slice2 = array_slice($arr, 4, 4);
+
+echo "<pre>";
+print_r($slice2);
+echo "<br>";
+
+$slice3 = array_slice($arr, 4);
+
+echo "<pre>";
+print_r($slice3);
+echo "<br>";
+
+$slice4 = array_slice($arr, 4, -3);
+
+echo "<pre>";
+print_r($slice4);
+echo "<br>";
