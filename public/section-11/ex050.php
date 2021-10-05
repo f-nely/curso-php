96804e0a29d7711b17e716860bd17c06f975041f
nely de faria <nelly.nf54@gmail.com>
2021-10-05 12:33:24 -0300

    Criando arquivo ex050.php

---
+++ public/section-11/ex050.php
@@ -0,0 +1,21 @@
+<?php
+
+$pessoas = [
+  'James Holden' => 32,
+  'Taylor Otwell' => 31
+];
+?>
+
+<table border="1">
+  <tr>
+    <th>Nome</th>
+    <th>Idade</th>
+  </tr>
+  <?php foreach($pessoas as $nome => $idade): ?>
+    <tr>
+      <td><?= $nome; ?></td>
+      <td><?= $idade; ?></td>
+    </tr>
+  <?php endforeach; ?>
+</table>
+
