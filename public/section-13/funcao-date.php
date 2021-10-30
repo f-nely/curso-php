5b259b45fe48006dd7cd7ed3d4bbd7e508b6d976
nely de faria <nelly.nf54@gmail.com>
2021-10-30 19:40:37 -0300

    Criando arquivo funcao-date.php

---
+++ public/section-13/funcao-date.php
@@ -0,0 +1,16 @@
+<?php
+
+date_default_timezone_set('America/Sao_Paulo');
+
+$d = date('d/m/y');
+
+echo $d . '<br>';
+
+//$d2 = date('D/M/Y');
+$d2 = date('d, M - Y');
+
+echo $d2 . '<br>';
+
+$d3 = date('l, F - Y H:i:s');
+
+echo $d3 . '<br>';
