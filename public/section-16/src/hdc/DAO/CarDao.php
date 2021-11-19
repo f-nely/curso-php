c2dbde20aaf18807ab43790d70ef6751f4f5779b
nely de faria <nelly.nf54@gmail.com>
2021-11-19 19:46:57 -0300

    Criando arquivo CarDao.php

---
+++ public/section-16/src/hdc/DAO/CarDao.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace HDC\DAO;
+
+use HDC\Models\Car;
+
+class CarDao implements ICarDao
+{
+  private $conn;
+
+  public function __construct(\PDO $conn)
+  {
+    $this->conn = $conn;
+  }
+
+  public function create(Car $car)
+  {
+    $stmt = $this->conn->prepare("INSERT INTO cars (brand, km, color) VALUES (:brand, :km, :color)");
+
+    $brand = $car->getBrand();
+    $km = $car->getKm();
+    $color = $car->getColor();
+
+    $stmt->bindParam(':brand', $brand);
+    $stmt->bindParam(':km', $km);
+    $stmt->bindParam(':color', $color);
+
+    $stmt->execute();
+  }
+
+  public function findAll()
+  {
+    $cars = [];
+
+    $stmt = $this->conn->query("SELECT * FROM cars");
+
+    $data = $stmt->fetchAll();
+
+    foreach ($data as $item) {
+      $car = new Car();
+
+      $car->setId($item['id']);
+      $car->setBrand($item['brand']);
+      $car->setKm($item['km']);
+      $car->setColor($item['color']);
+
+      $cars[] = $car;
+    }
+
+    return $cars;
+
+  }
+}
