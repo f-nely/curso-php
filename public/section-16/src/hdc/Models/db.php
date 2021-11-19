7357a320532d25b88bf0cba660e49c314b89b3af
nely de faria <nelly.nf54@gmail.com>
2021-11-19 19:48:35 -0300

    Criando arquivo db.php

---
+++ public/section-16/src/hdc/Models/db.php
@@ -0,0 +1,16 @@
+<?php
+
+$host = 'mysql';
+$dbname = 'daotest';
+$user = 'root';
+$pass = 'root';
+
+try {
+  $conn = new PDO("mysql:host=$host;dbname=$dbname", $user, $pass);
+
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+} catch (PDOException $e) {
+  $error = $e->getMessage();
+  echo "Erro: {$error}";
+}
