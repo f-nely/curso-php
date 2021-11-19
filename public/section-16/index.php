94fe3072e537af61ce79bb013fc898489ce1fca7
nely de faria <nelly.nf54@gmail.com>
2021-11-19 19:36:44 -0300

    Incluindo css

--- public/section-16/index.php
+++ public/section-16/index.php
@@ -16,6 +16,7 @@ require_once 'vendor/autoload.php';
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <!-- BOOTSTRAP -->
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/4.5.3/css/bootstrap.min.css" integrity="sha512-oc9+XSs1H243/FRN9Rw62Fn8EtxjEYWHXRvjS43YtueEewbS6ObfXcJNyohjHqVKFPoXXUxwc+q1K7Dee6vv9g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+  <link rel="stylesheet" href="css/styles.css">
   <title>Document</title>
 </head>
 <body>
